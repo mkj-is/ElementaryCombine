ba15abbf20695540b5e7ce72d3faa8044b58737a
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-11-07 21:57:17 +0100

    Add platform dependencies

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "ElementaryCombine",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .watchOS(.v6),
+        .tvOS(.v13)
+    ],
     products: [
         .library(
             name: "ElementaryCombine",
